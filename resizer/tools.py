997a3e5929acd2538712eb2bb9e36e0d581b6148
Grzegorz Śliwiński <fizyk@fizyk.dev>
2023-07-03 16:12:45 +0200

    Print out image size (bytes) changes summary after resize

---
+++ resizer/tools.py
@@ -0,0 +1,10 @@
+"""Additional tools to use."""
+
+
+def sizeof_fmt(num, suffix="B"):
+    """Format sizes with proper unit."""
+    for unit in ("", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"):
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
